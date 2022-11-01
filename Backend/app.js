eba96b299a67830fcf5fd152dfeb9c66a5d3f068
Hugo Abreu <hrabreu06@gmail.com>
2022-11-01 15:47:14 +0000

    CRUD TEST PASSED

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,35 @@ class MessageApp {
     constructor(){
         this.messages = []
 }
+
+post(content){
+    let item ={
+        id:this.messages.length,
+        content:content,
+        date: new Date()
+    }
+    this.messages.push(item)
+    return this.messages
 }
+
+get(id){
+    return this.messages[id];
+}
+
+update(id, update){
+    this.messages[0].content = update
+    return this.messages[id]
+
+}
+
+delete(id){
+this.messages.splice(id -1, 1)
+return this.messages
+}
+
+}
+
+
+
 
 export default MessageApp;
