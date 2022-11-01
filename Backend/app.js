5235c034fb95d35a5b321e06d61ab2907f69a192
Hugo Abreu <hrabreu06@gmail.com>
2022-11-01 22:59:41 +0000

    added filter and findIndex

--- Backend/app.js
+++ Backend/app.js
@@ -1,36 +1,39 @@
-class MessageApp {
-    constructor(){
-        this.messages = []
-}
-
-post(content){
-    let item ={
-        id:this.messages.length,
-        content:content,
-        date: new Date()
+function newId(array){
+    if (array.length > 0) {
+      return array[array.length-1].id + 1;
+    } else {
+      return 1
     }
-    this.messages.push(item)
+  }
+  
+  class MessageApp {
+    constructor() {
+      this.messages = []
+    }
+    post(message){
+      let item = {
+        id: newId(this.messages),
+        content: message,
+        date: new Date()
+      }
+      this.messages.push(item)
+      
+      return this.messages
+    }
+    
+    get(id){
+    return this.messages.filter(message => message.id === id)[0]
+    }
+    
+    update(id, update){
+    let index = this.messages.findIndex(message => message.id === id )
+    this.messages[index].content = update
+    }
+    
+    delete(id){
+    this.messages = this.messages.filter(message => message.id != id)
     return this.messages
-}
-
-get(id){
-    return this.messages[id];
-}
-
-update(id, update){
-    this.messages[0].content = update
-    return this.messages[id]
-
-}
-
-delete(id){
-this.messages.splice(id -1, 1)
-return this.messages
-}
-
-}
-
-
-
+    }
+   }
 
 export default MessageApp;
